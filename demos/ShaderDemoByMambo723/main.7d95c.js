5c336c95559c38c668688d4f52d2862742ffd150
TaoJiaheng <mambo723@163.com>
2019-09-25 22:05:02 +0800

    demo

--- demos/ShaderDemoByMambo723/main.7d95c.js
+++ demos/ShaderDemoByMambo723/main.7d95c.js
@@ -151,7 +151,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.935e3.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.13ea3.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -187,7 +187,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.d0db5.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.7d95c.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -214,12 +214,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.dcff1.js');
+        require('src/settings.0d5be.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.dcff1.js');
+        require('src/settings.0d5be.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
