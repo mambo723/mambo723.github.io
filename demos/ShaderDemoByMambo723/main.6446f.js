76d85b119bf6022e2c2084127c27a974ad43e06d
TaoJiaheng <mambo723@163.com>
2019-09-26 10:18:44 +0800

    shader

--- demos/ShaderDemoByMambo723/main.6446f.js
+++ demos/ShaderDemoByMambo723/main.6446f.js
@@ -187,7 +187,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.2092c.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.6446f.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -214,12 +214,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.e7535.js');
+        require('src/settings.bc123.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.e7535.js');
+        require('src/settings.bc123.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
